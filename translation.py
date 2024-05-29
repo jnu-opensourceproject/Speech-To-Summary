9bf0d40a0b0a9048ccca73c7409520447ae38b52
teataeteatae <112607034+teataeteatae@users.noreply.github.com>
2024-05-29 23:04:11 +0900

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+
+def translate_to_korean(sentence):
+    translator = Translator()
+    detected = translator.detect(sentence)
+    print(f"Detected language: {detected.lang}")
+    
+    result = translator.translate(sentence, dest='ko')
+    return result.text
+
+# 예시 사용
+sentence = "음성변환텍스트파일"
+translated_sentence = translate_to_korean(sentence)
+print(translated_sentence)
+
